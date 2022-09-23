91a9fce57563f1aa98a69665dc0f5c402cd62b3e
Aviva <avivajoy.gars@gmail.com>
2022-09-23 14:12:21 -0400

    trying again

--- function_c.py
+++ function_c.py
@@ -4,7 +4,7 @@ def merge_lists(list_a, list_b):
         without any duplicate elements.
     """
     print("Aviva's function C")
-
+    print("Trying one more time")
 
 if __name__ == "__main__":
     print(merge_lists([1, 1, 2, 3], [3, 4, 5]))
